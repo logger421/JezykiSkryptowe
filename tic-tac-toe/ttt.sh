4eb3e07eb548a274480d90159df693de10567359
Logger421 <wojteklotko3@gmail.com>
2023-11-27 22:40:04 +0100

    Added exiting from gameplay when user wants to save. Fixed displaying

--- tic-tac-toe/ttt.sh
+++ tic-tac-toe/ttt.sh
@@ -3,23 +3,25 @@
 GAMEPLAY_CHOICE_REGEX='^[0-9]$'
 GAMEPLAY_COMP_CHOICE_REGEX='^[1-9]$'
 MENU_CHOICE_REGEX='^[0-4]$'
-
+SAVE_AND_EXIT=false
 USER1_SYMBOL=X
 USER2_SYMBOL=O
 
 SAVES_DIR="$(dirname $0)/saves"
-echo "Saves dir: " SAVES_DIR
+echo "Saves dir: $SAVES_DIR"
 
 STATE=([1]=1 [2]=2 [3]=3 [4]=4 [5]=5 [6]=6 [7]=7 [8]=8 [9]=9)
 
 menu() {
-  echo "What you would like to do?"
-  echo "0. Exit"
-  echo "1. Player vs player tour game"
-  echo "2. Player vs computer tour game"
-  echo "3. Load previously saved game"
-  echo "Enter your choice: "
-  read MENU_CHOICE
+  echo "  ================================="
+  echo "  # What you would like to do?"
+  echo "  # 0. Exit"
+  echo "  # 1. Player vs player tour game"
+  echo "  # 2. Player vs computer tour game"
+  echo "  # 3. Load previously saved game"
+  echo "  ================================="
+  printf "  Enter your choice: "
+  read -r MENU_CHOICE
   if ! [[ $MENU_CHOICE =~ $MENU_CHOICE_REGEX ]]; then
     echo "Invalid input"
     menu
@@ -27,7 +29,9 @@ menu() {
 }
 
 draw_game() {
-  echo "USER1: ${USER1_SYMBOL}, USER2: ${USER2_SYMBOL}"
+  echo "\n===================="
+  echo "# USER1: ${USER1_SYMBOL}, USER2: ${USER2_SYMBOL}"
+  echo "===================="
   echo " -------------"
   echo " | ${STATE[7]} | ${STATE[8]} | ${STATE[9]} |"
   echo " |---+---+---|"
@@ -40,12 +44,11 @@ draw_game() {
 # This will save state to file
 save() {
   # Creat dir for saves
-  if [ ! -d "SAVES_DIR" ]; then
+  if [ ! -d "$SAVES_DIR" ]; then
     mkdir $SAVES_DIR
     echo "Created directory $SAVES_DIR"
   fi
   FILE_TIMESTAMP=$(date +%s)
-  echo "TIMESTAMP: $FILE_TIMESTAMP"
 }
 
 # This will load state of game from selected save
@@ -55,7 +58,8 @@ load() {
   else
     echo 'Choose file to load data:'
     ls -l "$SAVES_DIR"
-    read SAVE_FILENAME
+    printf "Enter filename: "
+    read -r SAVE_FILENAME
   fi
 }
 
@@ -64,19 +68,17 @@ end_game() {
   echo "======================"
   echo "The winner is: "
   if [[ $WINNER_SYMBOL == $USER1_SYMBOL && $USER2 != "COMPUTER" ]]; then
-    echo "USER1"
+    printf "USER1"
   else
-    echo "USER2"
+    printf "USER2"
   fi
 
   if [[ $WINNER_SYMBOL == $USER1_SYMBOL && $USER2 == "COMPUTER" ]]; then
-    echo "USER1"
+    printf "USER1"
   else
-    echo "COMPUTER"
+    printf "COMPUTER"
   fi
-
   echo "======================"
-  draw_game
   exit 0
 }
 
@@ -86,7 +88,7 @@ check_vertical_lines() {
       NEXTNEXT=$(($NEXT + 3))
       WINNER_SYMBOL=${STATE[$i]}
       [[ ${STATE[$i]} == ${STATE[$NEXT]} ]] && [[ ${STATE[$NEXT]} == ${STATE[$NEXTNEXT]} ]] && end_game $WINNER_SYMBOL
-    done
+  done
 }
 
 check_horizontal_lines() {
@@ -112,17 +114,28 @@ check_winner() {
 }
 
 player_move() {
-    printf "Enter your choice $2 [1-9] or press 0 to save and exit:"
-    read CHOICE
-    if ! [[ $CHOICE =~ $GAMEPLAY_CHOICE_REGEX ]]; then
-      echo "Invalid input"
-      player_move $1 $2
-    fi
-    if ! [[ ${STATE[$CHOICE]} =~ $GAMEPLAY_CHOICE_REGEX ]]; then
-      echo "Already occupied"
-      player_move $1 $2
-    fi
-    STATE[$CHOICE]=$1
+  printf "Enter your choice $2 [1-9] or press 0 to save and exit: "
+  read -r CHOICE
+
+  # check if input is valid
+  if ! [[ $CHOICE =~ $GAMEPLAY_CHOICE_REGEX ]]; then
+    echo "Invalid input"
+    player_move $1 $2
+  fi
+
+  # check if user pressed 0 then save and exit to main menu
+  if [[ $CHOICE == 0 ]]; then
+    save
+    SAVE_AND_EXIT=true
+    return 0
+  fi
+
+  # check if selected value was not selected
+  if ! [[ ${STATE[$CHOICE]} =~ $GAMEPLAY_CHOICE_REGEX ]]; then
+    echo "Already occupied"
+    player_move $1 $2
+  fi
+  STATE[$CHOICE]=$1
 }
 
 computer_move() {
@@ -135,9 +148,19 @@ computer_move() {
 vs_player() {
   while true; do
     player_move $USER1_SYMBOL 'USER1'
+    if $SAVE_AND_EXIT; then
+      SAVE_AND_EXIT=false
+      echo "Game saved. Exiting."
+      break
+    fi
     draw_game
     check_winner
     player_move $USER2_SYMBOL 'USER2'
+    if $SAVE_AND_EXIT; then
+      echo "Game saved. Exiting."
+      SAVE_AND_EXIT=false
+      break
+    fi
     draw_game
     check_winner
   done
@@ -155,8 +178,8 @@ vs_computer() {
   done
 }
 
-menu
 while true; do
+  menu
   if [[ $MENU_CHOICE == 0 ]]; then
       exit 0
   fi
