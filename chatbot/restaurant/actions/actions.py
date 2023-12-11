b4461fa31688e3d2f50835b51e4a3ad54c7cdb62
Logger421 <wojteklotko3@gmail.com>
2023-12-11 21:29:49 +0100

    Added list menu action

--- chatbot/restaurant/actions/actions.py
+++ chatbot/restaurant/actions/actions.py
@@ -11,12 +11,25 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
+from datetime import datetime
 
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import json
+
+
+def convert_time_to_int(time_str):
+    # Check if the time is in 12-hour format (contains AM or PM)
+    if 'AM' in time_str or 'PM' in time_str:
+        # Parse 12-hour format
+        parsed_time = datetime.strptime(time_str, '%I:%M%p')
+    else:
+        # Parse 24-hour format
+        parsed_time = datetime.strptime(time_str, '%H:%M')
+
+    return parsed_time.hour
+
 
 class ActionCheckOpeningHours(Action):
 
@@ -30,13 +43,13 @@ class ActionCheckOpeningHours(Action):
         requested_day = tracker.get_slot('day')
         requested_time = tracker.get_slot('time')
 
-        # Load opening hours from JSON file
-        with open('opening_hours.json') as json_file:
+        dispatcher.utter_message(text=f"Day: {requested_day}, time: {requested_time}, date: {requested_date}")
+
+        with open('actions/opening_hours.json') as json_file:
             data = json.load(json_file)
             hours = data['items'].get(requested_day, None)
 
-            # Check if the restaurant is open
-            if hours and hours['open'] <= requested_time <= hours['close']:
+            if hours and hours['open'] <= convert_time_to_int(requested_time) <= hours['close']:
                 dispatcher.utter_message(text=f"We're open on {requested_day} at {requested_time}.")
             else:
                 dispatcher.utter_message(text=f"We're closed on {requested_day} at {requested_time}.")
@@ -52,12 +65,10 @@ class ActionListMenu(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # Load menu items from JSON file
-        with open('menu.json') as json_file:
+        with open('actions/menu.json') as json_file:
             data = json.load(json_file)
             menu_items = data['items']
 
-            # Create a response with menu items
             response = "Here's our menu:\n"
             for item in menu_items:
                 response += f"- {item['name']}: ${item['price']}\n"
@@ -65,5 +76,3 @@ class ActionListMenu(Action):
             dispatcher.utter_message(text=response)
 
         return []
-
-# Define more custom actions as needed
--- chatbot/restaurant/main.py
+++ chatbot/restaurant/main.py
@@ -2,15 +2,22 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from datetime import datetime
 
+def convert_time_to_int(time_str):
+    # Check if the time is in 12-hour format (contains AM or PM)
+    if 'AM' in time_str or 'PM' in time_str:
+        # Parse 12-hour format
+        parsed_time = datetime.strptime(time_str, '%I:%M%p')
+    else:
+        # Parse 24-hour format
+        parsed_time = datetime.strptime(time_str, '%H:%M')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+    return parsed_time.hour
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    time = convert_time_to_int("6:30PM")
+    print(time)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
