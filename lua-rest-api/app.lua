4213e7f2f56b2421728311084e994014fc5c6b42
Logger421 <wojteklotko3@gmail.com>
2024-01-02 23:12:43 +0100

    Added two basic models and connection to db

--- lua-rest-api/app.lua
+++ lua-rest-api/app.lua
@@ -1,8 +1,39 @@
 local lapis = require("lapis")
+local Model = require("lapis.db.model").Model
+
 local app = lapis.Application()
 
-app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
+local Products = Model:extend("products", {
+  primary_key = "id"
+})
+
+local Categories = Model:extend("categories", {
+  primary_key = "id",
+  relations = {
+    { "products", has_many = "Products" }
+  }
+})
+
+app:get("/", function(self)
+  return "Welcome to Lapis "..require("lapis.version").." based shop"
+end)
+
+-- Get all categories
+app:get("/categories", function(self)
+  self.json = Categories:select()
+  return { json = self.json }
+end)
+
+-- Get all products
+app:get("/products", function(self)
+  self.json = Products:select()
+  return { json = self.json }
+end)
+
+app:match("/categories/:category", function(self)
+  local categoryDAO = Categories:find({ name = self.params.category})
+  self.json = categoryDAO:get_products()
+  return { json = self.json }
 end)
 
 return app
--- lua-rest-api/config.lua
+++ lua-rest-api/config.lua
@@ -3,5 +3,11 @@ local config = require("lapis.config")
 config("development", {
   server = "nginx",
   code_cache = "off",
-  num_workers = "1"
+  num_workers = "1",
+  mysql = {
+    host = "127.0.0.1",
+    user = "db-user",
+    password = "FILL_ME",
+    database = "FILL_ME"
+  }
 })
